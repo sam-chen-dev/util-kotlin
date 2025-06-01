d2c6628129b729f0f402cb70654b802e59ac0261
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2025-06-01 08:41:21 -0400

    Update: Library Version

--- utlikotlin/src/main/java/com/example/utlikotlin/RetrofitHelper.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/RetrofitHelper.kt
@@ -1,7 +1,9 @@
 package com.example.utlikotlin
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
 import retrofit2.Retrofit
 
 object RetrofitHelper {
@@ -11,6 +13,7 @@ object RetrofitHelper {
         encodeDefaults = true
     }
 
+    @OptIn(ExperimentalSerializationApi::class)
     private fun getRetrofit(baseUrl: String) = Retrofit.Builder().run {
         addConverterFactory(json.asConverterFactory(contentType))
         baseUrl(baseUrl)
