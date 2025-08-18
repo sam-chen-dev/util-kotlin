5937a5ca4be20538a812b1c16c99a0549bc998cc
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2025-08-18 17:56:23 -0400

    Add: QR Scan Feature using Google Code Scanner

--- utlikotlin/src/main/java/com/example/utlikotlin/QRCode.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/QRCode.kt
@@ -1,6 +1,10 @@
 package com.example.utlikotlin
 
+import android.content.Context
 import android.graphics.Bitmap
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.codescanner.GmsBarcodeScannerOptions
+import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
 import com.google.zxing.BarcodeFormat
 import com.journeyapps.barcodescanner.BarcodeEncoder
 
@@ -9,5 +13,19 @@ object QRCode {
         val encodedText = String(text.toByteArray(Charsets.UTF_8), Charsets.ISO_8859_1)
 
         return BarcodeEncoder().encodeBitmap(encodedText, BarcodeFormat.QR_CODE, width, height)
+    }
+
+    fun scan(context: Context, onResult: (String?) -> Unit) {
+        val scannerOptions = GmsBarcodeScannerOptions.Builder().run {
+            setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+            enableAutoZoom()
+            build()
+        }
+
+        val scanner = GmsBarcodeScanning.getClient(context, scannerOptions)
+
+        scanner.startScan().addOnSuccessListener { barcode ->
+            onResult(barcode.rawValue)
+        }
     }
 }
