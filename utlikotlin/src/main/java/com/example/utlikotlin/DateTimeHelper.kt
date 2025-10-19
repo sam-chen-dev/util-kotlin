ba7e2e3c58c784a95c23eb8379a6127b747b766c
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2025-10-19 14:00:18 -0400

    Add: Helper Function for Creating Epoch Millisecond from Given Data and Time

--- utlikotlin/src/main/java/com/example/utlikotlin/DateTimeHelper.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/DateTimeHelper.kt
@@ -1,7 +1,10 @@
 package com.example.utlikotlin
 
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
+import java.time.ZoneId
+import java.time.ZonedDateTime
 
 object DateTimeHelper {
     fun nowInSystemMillis() = LocalDateTime.now().toSystemMillis()
@@ -19,4 +22,14 @@ object DateTimeHelper {
     fun pastDayStartInSystemMillis(dayCount: Long) = LocalDateTime.now().minusDays(dayCount).with(LocalTime.MIN).toSystemMillis()
 
     fun pastDayEndInSystemMillis(dayCount: Long) = LocalDateTime.now().minusDays(dayCount).with(LocalTime.MAX).toSystemMillis()
+
+    fun createEpochMillis(year: Int, month: Int, day: Int, hour: Int, minute: Int): Long {
+        val localDate = LocalDate.of(year, month, day)
+        val localTime = LocalTime.of(hour, minute)
+        val zoneId = ZoneId.systemDefault()
+
+        val zoneDateTime = ZonedDateTime.of(localDate, localTime, zoneId)
+
+        return zoneDateTime.toInstant().toEpochMilli()
+    }
 }
