07a4ac2a0ed86efb6de68d8be7c1fa69e78bca72
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2025-10-29 11:38:57 -0400

    Remove Unused Class

--- utlikotlin/src/main/java/com/example/utlikotlin/BatteryHelper.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/BatteryHelper.kt
@@ -6,7 +6,7 @@ import android.content.IntentFilter
 import android.os.BatteryManager
 
 object BatteryHelper {
-    fun getIsCharging(context: Context): Boolean {
+    fun isCharging(context: Context): Boolean {
         val batteryStatus = IntentFilter(Intent.ACTION_BATTERY_CHANGED).let {
             context.registerReceiver(null, it)
         }
--- utlikotlin/src/main/java/com/example/utlikotlin/Decimal.kt
+++
@@ -1,6 +0,0 @@
-package com.example.utlikotlin
-
-object Decimal {
-    @JvmStatic
-    fun round(number: Double, digit: Int) = "%,.${digit}f".format(number)
-}
