65d366774bd696472cdc1a9503cbf2a1c9eca30b
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2025-12-01 09:40:58 -0500

    Update: Extension Fuction

--- utlikotlin/src/main/java/com/example/utlikotlin/ComposeExtensions.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/ComposeExtensions.kt
@@ -1,16 +1,18 @@
 package com.example.utlikotlin
 
-import android.graphics.Color
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.activity.compose.LocalActivity
 
-fun ComponentActivity.setSystemBarsStyle(isDarkStatusBar: Boolean, isDarkNavigationBar: Boolean) {
-    val lightSystemBarStyle = SystemBarStyle.light(Color.TRANSPARENT, Color.TRANSPARENT)
-    val darkSystemBarStyle = SystemBarStyle.dark(Color.TRANSPARENT)
+@Composable
+fun SetSystemBarsStyle(isDarkStatusBar: Boolean, isDarkNavigationBar: Boolean) {
+    val lightSystemBarStyle = SystemBarStyle.light(android.graphics.Color.TRANSPARENT, android.graphics.Color.TRANSPARENT)
+    val darkSystemBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT)
 
     val statusBarStyle = if (isDarkStatusBar) darkSystemBarStyle else lightSystemBarStyle
     val navigationBarStyle = if (isDarkNavigationBar) darkSystemBarStyle else lightSystemBarStyle
 
-    enableEdgeToEdge(statusBarStyle, navigationBarStyle)
+    (LocalActivity.current as ComponentActivity?)?.enableEdgeToEdge(statusBarStyle, navigationBarStyle)
 }
