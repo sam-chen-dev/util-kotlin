410dcc5ef2bb7bb914b33777bb5c71a17c4cfdd1
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2025-12-02 16:17:31 -0500

    Add: Composable Functions

--- utlikotlin/src/main/java/com/example/utlikotlin/ComposeUtils.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/ComposeUtils.kt
@@ -8,8 +8,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.LocalActivity
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.Dp
 
 @Composable
 fun SetSystemBarsStyle(isDarkStatusBar: Boolean, isDarkNavigationBar: Boolean) {
@@ -28,6 +32,12 @@ fun SimpleButton(text: String, modifier: Modifier = Modifier, onClick: () -> Uni
         Text(text)
     }
 }
+
+@Composable
+fun VerticalSpacer(value: Dp) = Spacer(Modifier.height(value))
+
+@Composable
+fun HorizontalSpacer(value: Dp) = Spacer(Modifier.width(value))
 
 fun showToast(context: Context, text: String) = Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
 
