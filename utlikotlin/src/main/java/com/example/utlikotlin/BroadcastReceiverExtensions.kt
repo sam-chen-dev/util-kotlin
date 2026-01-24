fe8ece9a14e13af40eaec66de913cd32966d777e
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-01-24 13:29:15 -0500

    Add: Extension Function for BroadcastReceiver

---
+++ utlikotlin/src/main/java/com/example/utlikotlin/BroadcastReceiverExtensions.kt
@@ -0,0 +1,22 @@
+package com.example.utlikotlin
+
+import android.content.BroadcastReceiver
+import android.content.BroadcastReceiver.PendingResult
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+@OptIn(DelicateCoroutinesApi::class)
+fun BroadcastReceiver.goAsync(action: suspend CoroutineScope.(PendingResult) -> Unit) {
+    val pendingResult = goAsync()
+
+    GlobalScope.launch(Dispatchers.IO) {
+        try {
+            action(pendingResult)
+        } finally {
+            pendingResult.finish()
+        }
+    }
+}
