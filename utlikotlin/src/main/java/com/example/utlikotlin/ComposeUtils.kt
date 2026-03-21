ef9df025b0e27b93ba4375e65b841278aa99ceed
Ziliang Chen (Sam) <c.ym62942@gmail.com>
2026-03-21 14:13:06 -0400

    Update ComposeUtils.kt

--- utlikotlin/src/main/java/com/example/utlikotlin/ComposeUtils.kt
+++ utlikotlin/src/main/java/com/example/utlikotlin/ComposeUtils.kt
@@ -1,35 +1,63 @@
 package com.example.utlikotlin
 
 import android.content.Context
-import androidx.compose.material3.Button
-import androidx.compose.material3.Text
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
 import androidx.activity.compose.LocalActivity
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LocalContentColor
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 
 @Composable
-fun SetSystemBarsStyle(isDarkStatusBar: Boolean, isDarkNavigationBar: Boolean) {
-    val lightSystemBarStyle = SystemBarStyle.light(android.graphics.Color.TRANSPARENT, android.graphics.Color.TRANSPARENT)
-    val darkSystemBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT)
-
-    val statusBarStyle = if (isDarkStatusBar) darkSystemBarStyle else lightSystemBarStyle
-    val navigationBarStyle = if (isDarkNavigationBar) darkSystemBarStyle else lightSystemBarStyle
-
-    (LocalActivity.current as ComponentActivity?)?.enableEdgeToEdge(statusBarStyle, navigationBarStyle)
+fun Text(resId: Int, color: Color = Color.Unspecified, fontSize: TextUnit = TextUnit.Unspecified) {
+    Text(stringResource(resId), color = color, fontSize = fontSize)
 }
 
 @Composable
+fun Text(text: String, color: Color = Color.Unspecified, fontSize: TextUnit = TextUnit.Unspecified) {
+    Text(text, color = color, fontSize = fontSize)
+}
+
+@Composable
+fun Icon(resId: Int, contentDescription: String, tint: Color = LocalContentColor.current) {
+    Icon(painterResource(resId), contentDescription, tint = tint)
+}
+
+@Composable
+fun IconButton(
+    resId: Int, contentDescription: String, onClick: () -> Unit, tint: Color = LocalContentColor.current
+) {
+    IconButton(onClick) {
+        Icon(resId, contentDescription, tint)
+    }
+}
+
+@Composable
+fun IconButton(
+    imageVector: ImageVector, contentDescription: String, onClick: () -> Unit, tint: Color = LocalContentColor.current
+) {
+    IconButton(onClick) {
+        Icon(imageVector, contentDescription, tint = tint)
+    }
+}
+
+@Composable
+fun IconButton(text: String, onClick: () -> Unit) {
+    IconButton(onClick) {
+        Text(text)
+    }
+}
+
+/*@Composable
 fun SimpleButton(text: String, modifier: Modifier = Modifier, onClick: () -> Unit) {
     Button(onClick, modifier) {
         Text(text)
@@ -57,7 +85,18 @@ fun PrimaryText(text: Any, fontSize: TextUnit, isBold: Boolean = false) {
 fun VerticalSpacer(value: Dp) = Spacer(Modifier.height(value))
 
 @Composable
-fun HorizontalSpacer(value: Dp) = Spacer(Modifier.width(value))
+fun HorizontalSpacer(value: Dp) = Spacer(Modifier.width(value))*/
+
+@Composable
+fun SetSystemBarsStyle(isDarkStatusBar: Boolean, isDarkNavigationBar: Boolean) {
+    val lightSystemBarStyle = SystemBarStyle.light(android.graphics.Color.TRANSPARENT, android.graphics.Color.TRANSPARENT)
+    val darkSystemBarStyle = SystemBarStyle.dark(android.graphics.Color.TRANSPARENT)
+
+    val statusBarStyle = if (isDarkStatusBar) darkSystemBarStyle else lightSystemBarStyle
+    val navigationBarStyle = if (isDarkNavigationBar) darkSystemBarStyle else lightSystemBarStyle
+
+    (LocalActivity.current as ComponentActivity?)?.enableEdgeToEdge(statusBarStyle, navigationBarStyle)
+}
 
 fun showToast(context: Context, text: String) = Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
 
